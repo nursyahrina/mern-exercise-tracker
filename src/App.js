d73c884d0ec0c47d9e4377431f784bdb095bb591
nursyahrina <nursyahrina17@gmail.com>
2022-07-24 07:39:42 +0700

    resolve params problem in react v6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Route, Routes, useParams,
+} from 'react-router-dom';
 
 import Navbar from './components/navbar.component';
 import ExerciseList from './components/exercise-list.component';
@@ -7,13 +9,19 @@ import EditExercise from './components/edit-exercise.component';
 import CreateExercise from './components/create-exercise.component';
 import CreateUser from './components/create-user.component';
 
+function Wrapper(props) {
+  const params = useParams();
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <EditExercise {...{ ...props, match: { params } }} />;
+}
+
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" exact element={<ExerciseList />} />
-        <Route path="/edit:id" element={<EditExercise />} />
+        <Route path="/edit/:id" element={<Wrapper />} />
         <Route path="/create" element={<CreateExercise />} />
         <Route path="/user" element={<CreateUser />} />
       </Routes>
